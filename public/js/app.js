063b1e108a511d86a5723ab4fb1fd9821bef6309
Tyler Negro <TylerNegro@tylers-mbp.uconnect.utah.edu>
2017-09-14 21:27:04 -0600

    added form and post works

--- public/js/app.js
+++ public/js/app.js
@@ -1,13 +1,33 @@
 $(document).ready(function(){
 
-	console.log("Hello!")
-	
-$.get('/api/tables', function(data){
-	console.log(data);
-})
+		console.log("Hello!")
+		
+	$.get('/api/tables', function(data){
+		console.log(data);
+	})
 
-$('body').on('click', function(){
-	console.log('stuff')
-})
+	$('body').on('click', function(){
+		console.log('stuff')
+	})
 
+
+	/* THIS HANDLES THE RESERVATION FORM AND POSTS IT TO THE ARRAY */
+	$('#add-reservation').on('click',function(event){
+		event.preventDefault();
+		var newReservation = {
+			customerName: $('#form-name').val().trim(),
+			phoneNumber: $('#form-phone').val().trim(),
+			customerEmail: $('#form-email').val().trim(),
+			customerID: $('#form-id').val().trim()
+		}
+		console.log(newReservation)
+		
+		$.post("/api/new", newCharacter)
+			.done(function(data) {
+				  console.log(data);
+			  alert("Adding character...");
+		});
+	})
+
+	//////////////////////////////////////////////////////////////////
 })
