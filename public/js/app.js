3330847985288187c051c5f46f9aff2c71981024
Joe Gallegos <joebgallegos@gmail.com>
2017-09-14 21:26:43 -0600

    Dynamic table

--- public/js/app.js
+++ public/js/app.js
@@ -3,7 +3,23 @@ $(document).ready(function(){
 	console.log("Hello!")
 	
 $.get('/api/tables', function(data){
-	console.log(data);
+	for (var i = 0; i < data.length; i++){
+	var person = data[i].customerName;
+	console.log(person);
+
+	$("#tableBody").append("<tr>" + "<td>" + person + "</td>" + "</tr>");
+
+	}
+
+	// $(function(){
+	// 	$.each(person, function(value) {
+	// 		console.log(key);
+	// 		var $tr = $('<tr>').append(
+	// 			// $('<td>').text(key),				
+	// 			$('<td>').text(value)
+	// 		).appendTo('.table')
+	// 	})
+	// })
 })
 
 $('body').on('click', function(){
